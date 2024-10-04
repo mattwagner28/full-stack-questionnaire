8692e8c944a38616c8067cdd5245e8c5b1256f9e
MattWagner28 <matthewwagner@outlook.com>
2024-10-04 11:39:17 -0400

    updated db queries file

--- db/mutations.sql
+++ db/mutations.sql
@@ -9,6 +9,20 @@ UPDATE users
 SET password = $2
 WHERE username = $1;
 
-// Save intake form
-INSERT INTO questionnaire_submissions (user_id, questionnaire_id, answers)
-VALUES ($1, $2, $3);
+
+// if user exists, update the password just so they can login easily
+
+UPDATE users
+SET password = $2
+
+// if answer has been submitted, update the questionnaire and the timestamp
+
+UPDATE questionnaire_answers 
+SET answer = $1, created_at = CURRENT_TIMESTAMP
+WHERE user_id = $2 
+AND questionnaire_id = $3 
+AND question_id = $4;
+
+// if answer has not been submitted, save a new answer
+INSERT INTO questionnaire_answers (user_id, questionnaire_id, question_id, answer) 
+VALUES ($1, $2, $3, $4);
